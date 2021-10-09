ea9d90afbb2daff1c91a60391ed7a5aee6a7c30c
jakeschultz89 <ejacobschultz@gmail.com>
2021-10-09 11:52:28 -0400

    assignment 1

---
+++ src/components/footer.jsx
@@ -0,0 +1,12 @@
+import "./footer.css";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <h5>Online Store. 2021</h5>
+      <h6>Jake Schultz</h6>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ src/components/navBar.jsx
@@ -0,0 +1,11 @@
+import "./navBar.css";
+
+function NavBar() {
+  return (
+    <div className="nav">
+      <h3>Menu will be here!</h3>
+    </div>
+  );
+}
+
+export default NavBar;
